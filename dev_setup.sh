83937dc6f99e604b38792b1e166c1c2cb94c5c49
Antonio Garrote <antoniogarrote@gmail.com>
2017-03-20 23:22:22 +0000

    Adding development setup script

---
+++ dev_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+lein npm install
+lein cljsbuild once default
+ln -s $(pwd)/js $(pwd)/node/js
+cd api-modeller
+tsc
+npm install
+cd ..
+ln -s $(pwd)/node/engine $(pwd)/api-modeller/node_modules/api_modelling_framework
+cp package_files/* api-modeller/node_modules/api_modelling_framework/
+pushd api-modeller/public
+bower install
+popd
+cd api-modeller && gulp serve
