08e446933e4b0f91852a4dbb1abcc4d2bfb5ef9a
Antonio Garrote <antoniogarrote@gmail.com>
2017-05-19 14:41:47 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject api-modeling-framework "0.1.2-SNAPSHOT"
+(defproject api-modeling-framework "0.1.3-SNAPSHOT"
 
   :description "API and domain modeling tools for RAML, OpenAPI (Swagger) and RDF"
 
